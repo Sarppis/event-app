a883c556c2e227143983c92d6cebd0eddd659b81
Harju Riikka <s1900157@edu.bc.fi>
2019-06-03 13:36:43 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState }  from 'react';
 import Content from './Content';
 import Header from './Header';
+import Buttons from './Buttons';
 
 const App = () => {
+  const [index, setIndex] = useState(0)
+  const handleClick = () => {
+    setIndex(index+1);
+    console.log('clicked!');
+}
   return (
     <div className="App">
     <Header />
+    <main>
+    <Buttons onClick={handleClick} icon="heart-broken" />
+    <Buttons onClick={handleClick} icon="heart" />
+      </main>   <main>
+    <Buttons onClick={handleClick} icon="heart-broken" />
+    <Buttons onClick={handleClick} icon="heart" />
+    </main>   <main>
+    <Buttons onClick={handleClick} icon="heart-broken" />
+    <Buttons onClick={handleClick} icon="heart" />
+    </main>
     <Content />
     </div>
   );
