1badc73a211dd6060aa401ae861218fc76aaa5a1
SannaSarpaneva <Sarppis@users.noreply.github.com>
2019-06-05 10:56:02 +0300

    buttons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect }  from 'react';
 import Content from './Content';
 import Header from './Header';
+import Buttons from './Buttons';
 
 const App = () => {
+  const [index, setIndex] = useState(0)
+  const [data, setData] = useState([])
+  const handleClick = () => {
+    setIndex(index+1);
+    console.log('clicked!');
+}
+
+useEffect(() => {
+const getEvents = () => {
+  fetch('http://localhost:3030/events/?language_filter=en&limit=100')
+.then(function(response) {
+ return response.json();
+})
+.then(function(myJson) {
+ console.log(JSON.stringify(myJson));
+});
+}}, [])
+
   return (
     <div className="App">
-    <Header />
+  <Header title='HeL EVENTS' />
+    <main>
+    <Buttons onClick={handleClick} icon="basketball" />
+    <Buttons onClick={handleClick} icon="music" />
+      </main>   <main>
+    <Buttons onClick={handleClick} icon="kids" />
+    <Buttons onClick={handleClick} icon="concert" />
+    </main>   <main>
+      <Buttons onClick={handleClick} icon="festival" />
+    <Buttons onClick={handleClick} icon="teater" />
+    </main>
     <Content />
     </div>
   );
--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import balloons from './img/balloons.png';
 
-const Header = () => {
+
+const Header = ({ title }) => {
   return (
-  <header>
-      Our App
+  <div>
+    <header>
+      { title }
     </header>
-  );
+    <div className="image">
+      <img src={ balloons } alt="dark" />
+    </div>
+  </div>
+)
+
 };
 
 export default Header;
