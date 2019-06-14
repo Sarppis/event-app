2d9a0417f496aec07b88b4628f30816cda4fd1c0
SannaSarpaneva <Sarppis@users.noreply.github.com>
2019-06-14 10:52:01 +0300

    new

--- src/App.js
+++ src/App.js
@@ -1,58 +1,109 @@
-import React, { useState, useEffect }  from 'react';
+import React, { useState, useEffect } from 'react';
 import Content from './Content';
 import Header from './Header';
 import Buttons from './Buttons';
-
 
 const App = () => {
   const [index, setIndex] = useState(0);
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(true);
   const handleClick = () => {
-    setIndex(index+1);
+    setIndex(index + 1);
     console.log('clicked!');
-};
+  };
+  useEffect(() => {
+    if (loading) {
+      fetch('http://localhost:3030/api/events/')
+        .then(res => res.json())
+        .then((myData) => {
+          setLoading(false);
+          setEvents(myData.data);
+          console.log(typeof(myData.data))
+        }).catch(console.log)
+    }
+  })
 
-useEffect(() => {
-  if (loading) {
-    fetch('http://localhost:3030/api/events/')
-    .then(res => res.json())
-    .then((myData) => {
-      setLoading(false);
-      setEvents(myData.data);
+  const handleClickSports = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=sports').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
-      console.log(typeof(myData.data))
-    })
+  const handleClickMusic = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=music').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
-    .catch(console.log)
+  const handleClickKids = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=families').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
-  }})
+  const handleClickMusicals = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=musicals').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
+  const handleClickFestival = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=festivals').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
+  const handleClickTeatre = () => {
+    fetch('http://localhost:3030/api/events/?tags_search=theatre').then(res => res.json())
+      .then((myData) => {
+        setLoading(false);
+        setEvents(myData.data);
+      }).catch(function(err) {
+      });
+  };
 
-    return (
-      <React.Fragment>
-        { loading || !events ? ( <div> Loading.... </div>
+  return (
+    <React.Fragment>
+    {
+      loading || !events ? ( < div > Loading.... < /div>
       ) : (
-      <div className="App">
-        <Header title='HeL EVENTS'/>
-        <div className='action'>
+        <div className = "App">
+        < Header title = 'HeL EVENTS' / >
+        <div className = 'action'>
         <main>
-          <Buttons onClick={handleClick} buttonTitle=<p>Sport</p> icon="basketball" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Music</p> icon="music" />
-          <Buttons onClick={handleClick} buttonTitle=<p> Kids</p> icon="kids" />
+        < Buttons onClick = { handleClickSports } buttonTitle = <p>Sport</p> icon="basketball" />
+        < Buttons onClick = { handleClickMusic } buttonTitle = <p>Music</p> icon="music" />
+        < Buttons onClick = { handleClickKids } buttonTitle = <p>Family</p> icon="kids" />
         </main>
         <main>
-          <Buttons onClick={handleClick} buttonTitle=<p>Concert</p>icon="concert" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Festival</p> icon="festival" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Teater</p> icon="teater" />
+        < Buttons onClick = { handleClickMusicals } buttonTitle = <p>Concert</p> icon="concert" />
+        < Buttons onClick = { handleClickFestival } buttonTitle = <p>Festival</p> icon="festival" />
+        < Buttons onClick = { handleClickTeatre } buttonTitle = <p>Theater</p> icon="teater" />
+        </main>
+        <main>
+        < Buttons onClick = { handleClickTeatre } buttonTitle = <p>All the events</p>  />
         </main>
         </div>
-        <Content events={events} />
-      </div>
-      )}
+        <Content events = { events } />
+        </div>
+      )
+    }
     </React.Fragment>
   )
 }
-
 export default App;
