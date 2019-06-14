4c3a0b81f3487792ea3c481966ee5b777286dda9
Harju Riikka <s1900157@edu.bc.fi>
2019-06-14 09:38:40 +0300

    tag filtering added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ const App = () => {
 
 useEffect(() => {
   if (loading) {
-    fetch('http://localhost:3030/')
+    fetch('http://localhost:3030/api/events/')
     .then(res => res.json())
     .then((myData) => {
       setLoading(false);
@@ -26,9 +26,54 @@ useEffect(() => {
     .catch(console.log)
 
   }})
-
-
-
+  const handleClickSports = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=sports')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
+  const handleClickMusic = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=music')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
+  const handleClickKids = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=families')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
+  const handleClickMusicals = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=musicals')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
+  const handleClickFestival = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=festivals')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
+  const handleClickTeatre = () => {
+  fetch('http://localhost:3030/api/events/?tags_search=theatre')  .then(res => res.json())
+    .then((myData) => {
+      setLoading(false);
+      setEvents(myData.data);
+    }).catch(function(err) {
+  });
+  };
     return (
       <React.Fragment>
         { loading || !events ? ( <div> Loading.... </div>
@@ -37,14 +82,14 @@ useEffect(() => {
         <Header title='HeL EVENTS'/>
         <div className='action'>
         <main>
-          <Buttons onClick={handleClick} buttonTitle=<p>Sport</p> icon="basketball" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Music</p> icon="music" />
-          <Buttons onClick={handleClick} buttonTitle=<p>_Kids</p> icon="kids" />
+          <Buttons onClick={handleClickSports} buttonTitle=<p>Sport</p> icon="basketball" />
+          <Buttons onClick={handleClickMusic} buttonTitle=<p>Music</p> icon="music" />
+          <Buttons onClick={handleClickKids} buttonTitle=<p>Family</p> icon="kids" />
         </main>
         <main>
-          <Buttons onClick={handleClick} buttonTitle=<p>Concert</p>icon="concert" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Festival</p> icon="festival" />
-          <Buttons onClick={handleClick} buttonTitle=<p>Teater</p> icon="teater" />
+          <Buttons onClick={handleClickMusicals} buttonTitle=<p>Musicals</p>icon="concert" />
+          <Buttons onClick={handleClickFestival} buttonTitle=<p>Festival</p> icon="festival" />
+          <Buttons onClick={handleClickTeatre} buttonTitle=<p>Theater</p> icon="teater" />
         </main>
         </div>
         <Content events={events} />
